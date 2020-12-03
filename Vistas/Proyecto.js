b51e4bdc1b45cd6ac48110d17db0d1ec1182e773
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-12-03 15:32:56 -0300

    avances pantalla proyecto

--- Vistas/Proyecto.js
+++ Vistas/Proyecto.js
@@ -1,4 +1,45 @@
 export const Proyecto =
     `<div>
-    <p>Aquí van los proyectos jiji<p>
-    <div> `     
+    <figure class="snip1295"><img src="./Imagen/comidaparatodos.png" alt="sample67"/>
+  <div class="border one">
+    <div></div>
+  </div>
+  <div class="border two">
+    <div></div>
+  </div><a href="https://comidaparatodosreactivas.web.app/"></a>
+</figure>
+<figure class="snip1295 hover"><img src="./Imagen/datalovers.png" alt="sample84"/>
+  <div class="border one">
+    <div></div>
+  </div>
+  <div class="border two">
+    <div></div>
+  </div><a href="https://maritz-u.github.io/SCL014-data-lovers/src/index.html"></a>
+</figure>
+<figure class="snip1295"><img src="./Imagen/páginaReia.png" alt="sample68"/>
+  <div class="border one">
+    <div></div>
+  </div>
+  <div class="border two">
+    <div></div>
+  </div><a href="https://cafeteriareia-6c8ba.web.app/"></a>
+</figure>
+    <div>
+    <div>
+    <figure class="snip1295"><img src="./Imagen/facilito.png" alt="sample67"/>
+  <div class="border one">
+    <div></div>
+  </div>
+  <div class="border two">
+    <div></div>
+  </div><a href="http://facilito.online/"></a>
+</figure>
+    `     
+
+/* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+  

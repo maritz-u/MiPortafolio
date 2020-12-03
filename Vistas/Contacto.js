641e24fb9d1178128d1d5638137dc4068ce9b806
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-12-03 13:36:27 -0300

    arreglos pantalla de contacto

--- Vistas/Contacto.js
+++ Vistas/Contacto.js
@@ -1,4 +1,22 @@
 export const Contacto = 
     `<div>
-    <p>Aquí van los proyectos jiji<p>
+    <figure class="snip0041">
+  <img src="./Imagen/telefono.jpg" alt="sample5"/> 
+  <div class="icons">
+    <a href="#"><i class="ion-ios-home-outline"></i></a>
+    <a href="#"><i class="ion-ios-email-outline"></i></a>
+    <a href="#"><i class="ion-ios-telephone-outline"></i></a>
+  </div>
+  <figcaption>
+    <h2>Maritzu <span>Contacto</span></h2>
+    <p>Te interesa trabajar, ¡Conéctemos!</p>
+  </figcaption> 
+</figure>
     <div> `
+
+/* Demo purposes only */  
+$("figure").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
--- Vistas/Sobremi.js
+++ Vistas/Sobremi.js
@@ -1,6 +1,26 @@
 export const Sobremi = 
-    `<div class= contenedor>
-    <p>Me llamo Maritzu Zúñiga y soy Front-end Developer<p>
-    <p>me apasiona bshdkj<p>
-    <div> `
+    `<figure class="snip1515">
+    <div class="profile-image"><img src="./Imagen/imgsobremi.jpg" alt="sample47" /></div>
+    <figcaption>
+      <h3>Maritzu Zúñiga</h3>
+      <h4>Developer</h4>
+      <p>"A partir de cierto punto ya no hay vuelta atrás. Hay que llegar a ese punto."</p>
+      <div class="icons"><a href="#"><i class="ion-social-reddit"></i></a>
+        <a href="#"> <i class="ion-social-twitter"></i></a>
+        <a href="#"> <i class="ion-social-vimeo"></i></a>
+      </div>
+    </figcaption>
+  </figure>
+  <div class="description">
+  <h2>Mi nombre es Maritzu y me dedico al desarrollo</h2>
+  <h2>de soluciones tecnológicas. Me encantan los desafíos y</h2>
+  <h2>aprender. Soy muy curiosa y proactiva, creo en la búsqueda</h2>
+  <h2>constante del conocimiento como forma de vida.</h2>
+  </div>`
  
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );

aecbb05eac128e12a4e5af73ade16fc6399b6f21
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-12-22 16:41:18 -0300

    ultimos arreglos

--- Vistas/Proyecto.js
+++ Vistas/Proyecto.js
@@ -1,48 +1,52 @@
 export const Proyecto = `
   <figure class="contenedorProyecto">
-  <h4>Comida para todos </h4>
   <div class="image">
     <img src="./Imagen/comidaparatodos.png" alt="sq-sample4"/>
   </div>
   <figcaption>
     <p>
-      I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life.
+    Trabajo en colaboración con la Fundación "Comida para todos" en 
+    el cual se buscó diseñar y desarrollar su interfaz web. 
+    El sitio fue implementado con Reactjs. 
     </p>
   </figcaption><a href="https://comidaparatodosreactivas.web.app/" class="add-to-cart">Visualizar</a>
 </figure>
 
 <figure class="contenedorProyecto">
-<h4>PokemonLover's </h4>
 <div class="image">
   <img src="./Imagen/pokebola.png" alt="sq-sample4"/>
 </div>
 <figcaption>
   <p>
-    I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life.
+  Es uno de los más grandes y populares universos de cuantos se han creado en la historia de los videojuegos. 
+  En esta página te entregaremos una guía visible sobre la generación Kanto.
+  Vamos a explicarte qué son los Pokémon y cómo se clasifican.
   </p>
 </figcaption><a href="https://maritz-u.github.io/SCL014-data-lovers/src/index.html" class="add-to-cart">Visualizar</a>
 </figure>
 
 <figure class="contenedorProyecto">
-<h4>Cafetería-Reía</h4>
 <div class="image">
   <img src="./Imagen/reia.png" alt="sq-sample4"/>
 </div>
 <figcaption>
   <p>
-    I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life.
+  Aplicación web implementada con React y Firebase para Ipad. 
+  En está aplicación buscamos una solución tecnológica 
+  para automatizar el canal de comunicación entre los meseros y 
+  cocineros de una Cafetería.
   </p>
 </figcaption><a href="https://cafeteriareia-6c8ba.web.app/" class="add-to-cart">Visualizar</a>
 </figure>
     
 <figure class="contenedorProyecto">
-<h4>FacilitoApp</h4>
 <div class="image">
   <img src="./Imagen/facilito.png" alt="sq-sample4"/>
 </div>
 <figcaption>
   <p>
-    I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life.
+  Proyecto en conjunto a Barros&Errázuriz para la LegalHackathon. 
+  Proyecto implementado con Reactjs y Material ui.
   </p>
 </figcaption><a href="http://facilito.online/" class="add-to-cart">Visualizar</a>
 </figure>`;
--- Vistas/Sobremi.js
+++ Vistas/Sobremi.js
@@ -1,13 +1,15 @@
 export const Sobremi = `
+<div class= "contenedorSobreMi">
     <figure class="snip1515">
     <div class="profile-image"><img src="./Imagen/imgsobremi.jpg" alt="sample47" /></div>
     <figcaption>
       <h3>Maritzu Zúñiga</h3>
       <h4>Developer</h4>
       <p>"A partir de cierto punto ya no hay vuelta atrás. Hay que llegar a ese punto."</p>
-      <div class="icons"><a href="https://www.linkedin.com/in/maritzu-z%C3%BA%C3%B1iga/"><i class="ion-social-linkedin"></i></a>
-        <a href="https://github.com/maritz-u"> <i class="ion-social-github"></i></a>
-        <a href="#"> <i class="ion-social-vimeo"></i></a>
+      <div class="icons">
+      <a href="https://www.linkedin.com/in/maritzu-z%C3%BA%C3%B1iga/"><i class="ion-social-linkedin"></i></a>
+      <a href="mailto:maritzudelosangeles@gmail.com"> <i class="ion-ios-email"></i></a> 
+      <a href="https://github.com/maritz-u"> <i class="ion-social-github"></i></a>
       </div>
     </figcaption>
   </figure>
@@ -16,4 +18,5 @@ export const Sobremi = `
   <h2>de soluciones tecnológicas. Me encantan los desafíos y</h2>
   <h2>aprender. Soy muy curiosa y proactiva, creo en la búsqueda</h2>
   <h2>constante del conocimiento como forma de vida.</h2>
+  </div>
   </div>`;
